696e4bd1e71c30368b522e0ecfb4c01296049e51
dhananjaymaur99 <dhananjay.99jb@gmail.com>
2025-10-23 10:31:04 +0530

    token prefix changed to Bearer

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,35 +9,32 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    
     public function store(UserStoreRequest $request)
-    {   
+    {
 
-        $data = ["name" => $request->name, "email" => $request->email, "password" => Hash::make($request->password)];
+        $data = ['name' => $request->name, 'email' => $request->email, 'password' => Hash::make($request->password)];
         $user = User::create($data);
-        $token = $user->createToken('auth-token',['*'], now()->addWeek())->plainTextToken;
+        $token = $user->createToken('Bearer', ['*'], now()->addWeek())->plainTextToken;
 
-        return response()->json(["user" => $user, "token" => $token], 201);
+        return response()->json(['user' => $user, 'token' => $token], 201);
     }
 
     public function login(Request $request)
     {
         $user = User::where('email', $request->email)->first();
 
-        if(!$user){
-            return response()->json(["message" => "User not found"], 404);
+        if (! $user) {
+            return response()->json(['message' => 'User not found'], 404);
         }
 
         $isCorrect = Hash::check($request->password, $user->password);
 
-
-        if(!$isCorrect){
-            return response()->json(["message" => "Incorrect credentials"], 401);
+        if (! $isCorrect) {
+            return response()->json(['message' => 'Incorrect credentials'], 401);
         }
 
-        $token = $user->createToken('auth-token',['*'], now()->addWeek())->plainTextToken;
+        $token = $user->createToken('auth-token', ['*'], now()->addWeek())->plainTextToken;
 
-        return response()->json(["token" => $token], 200);
+        return response()->json(['token' => $token], 200);
     }
-
 }
